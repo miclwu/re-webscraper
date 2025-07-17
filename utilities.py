9c7cab33ee7ac5d9414f48e65288c983ff8509a9
Michael Wu <michael.cj.wu@gmail.com>
2025-07-16 17:40:56 -0700

    feat: add csv/json utilities

---
+++ utilities.py
@@ -0,0 +1,27 @@
+import csv
+import json
+
+def csv_to_dict(infile):
+    data = []
+    with open(infile, 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            data.append(row)
+    return data
+
+def dict_to_csv(data, outfile, fieldnames):
+    with open(outfile, 'w') as f:
+        writer = csv.DictWriter(f, fieldnames)
+        
+        writer.writeheader()
+        writer.writerows([row for row in data])
+
+def json_to_dict(infile):
+    with open(infile, 'r') as f:
+        dictobj = json.load(f)
+    return dictobj
+
+def dict_to_json(dictobj, outfile, indent=4):
+    with open(outfile, 'w') as f:
+        json.dump(dictobj, f, indent=indent)
+
