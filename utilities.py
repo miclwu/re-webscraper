ac47eec0ca05a64293ab80e51d6a23ec1122f452
Michael Wu <michael.cj.wu@gmail.com>
2025-07-24 16:17:30 -0700

    feat: add sql db table to py dict converter

--- utilities.py
+++ utilities.py
@@ -1,6 +1,33 @@
+import sqlite3
 import csv
 import json
 
+class InvalidInputError(ValueError):
+    """Errors raised for invalid input"""
+    pass
+
+# from https://docs.python.org/3/library/sqlite3.html#sqlite3-howto-row-factory
+def db_dict_factory(cursor, row):
+    fields = [column[0] for column in cursor.description]
+    return {key: value for key, value in zip(fields, row)}
+
+def dbtable_to_dict(database, table):
+    data = []
+    conn = sqlite3.connect(f"file:{database}?mode=ro", uri=True)
+    cur = conn.cursor()
+    res = cur.execute("SELECT name FROM sqlite_master")
+    if (table,) not in res.fetchall():
+        conn.close()
+        raise InvalidInputError(f"Table '{table}' not found")
+    
+    cur.row_factory = db_dict_factory
+    cur.execute(f"SELECT * FROM {table}")
+
+    for row in cur:
+        data.append(row)
+
+    conn.close()
+    return data
 def csv_to_dict(infile):
     data = []
     with open(infile, 'r') as f:
