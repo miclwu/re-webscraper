41e2227a53f3f9dc7036738763a7798a24494cd7
Michael Wu <michael.cj.wu@gmail.com>
2025-08-10 02:48:18 -0700

    feat: add users table operations, refactor into separate functions

--- setup.py
+++ setup.py
@@ -1,31 +1,82 @@
 import sqlite3
-from utilities import csv_to_dict
+from utilities import csv_to_records
+from constants import DATABASE, EMAIL_ADDRESS
+from typing import Any
 
-conn = sqlite3.connect("webscraper.db")
-cur = conn.cursor()
+def init_table_funds(
+    conn: sqlite3.Connection
+) -> None:
+    """Initialize `funds` table in database
 
-cur.execute("DROP TABLE IF EXISTS funds")
-
-cur.execute("""
-    CREATE TABLE funds (
+    Args:
+        conn: An open connection to an sqlite3 database
+    """
+    cur = conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS funds")
+    cur.execute(
+    """CREATE TABLE funds (
         id INTEGER NOT NULL PRIMARY KEY,
         name TEXT UNIQUE NOT NULL,
         url TEXT NOT NULL,
         status TEXT NOT NULL,
         checksum TEXT,
-        access_failures INTEGER DEFAULT 0)"""
-)
+        urls_to_check TEXT,
+        access_failures TINYINT DEFAULT 0)
+    """
+    )
+    conn.commit()
 
-data = csv_to_dict("database_BACKUP.csv")
+def init_table_users(
+    conn: sqlite3.Connection
+) -> None:
+    """Initialize `users` table in database
 
-cur.executemany("INSERT INTO funds (name, url, status, checksum) VALUES (:name, :url, :status, :checksum)", data)
-conn.commit()
+    Args:
+        conn: An open connection to an sqlite3 database
+    """
+    cur = conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS users")
+    cur.execute(
+    """CREATE TABLE users (
+        id INTEGER NOT NULL PRIMARY KEY,
+        email TEXT UNIQUE NOT NULL,
+        admin BOOL DEFAULT 0)
+    """
+    )
+    conn.commit()
 
-res = cur.execute("SELECT name FROM funds")
-print(res.fetchall())
+def add_user(
+    conn: sqlite3.Connection,
+    email: str,
+    admin: bool
+) -> None:
+    """Add user to `users` table in database.
 
+    Args:
+        conn: An open connection to an sqlite3 database
+        email: The email of the user to add
+        admin: The privilege of the user; `True` if admin, `False` otherwise
+    """
+    cur = conn.cursor()
+    cur.execute('INSERT INTO users (email, admin) VALUES (?, ?)', (email, admin))
+    cur.commit()
 
-res = cur.execute("SELECT name FROM sqlite_master")
-print(res.fetchall())
+def add_funds(
+    conn: sqlite3.Connection,
+    records: list[dict[str, Any]]
+) -> None:
+    """Add funds from `records` object to `funds` table in database.
 
-conn.close()
+    Args:
+        conn: An open connection to an sqlite3 database
+        records: A list of dicts, with each dict representing a row in database
+    """
+    cur = conn.cursor()
+    cur.executemany("INSERT INTO funds (name, url, status, checksum) VALUES (:name, :url, :status, :checksum)", records)
+    conn.commit()
+
+if __name__ == '__main__':
+    conn = sqlite3.connect(DATABASE)
+    init_table_users(conn)
+    add_user(conn, EMAIL_ADDRESS, True)
+    conn.close()
